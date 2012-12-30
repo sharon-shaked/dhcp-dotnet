288c1e3b5d7ec7741b69f38c18f2b42b023e9ea9
roger.castaldo@gmail.com <roger.castaldo@gmail.com@c2fbc150-0ca7-5545-2453-33d09627541d>
2012-12-30 17:57:36 +0000

    moving code around for cleaner code base and unnecessary additional namespacing.

--- trunk/Structures.cs
+++ trunk/Structures.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.IO;
 using System.Net;
 
-namespace Org.Reddragonit.Net.DHCP.Components
+namespace Org.Reddragonit.Net.DHCP
 {
 
     public struct DHCPOption : IComparable
